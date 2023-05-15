7738a4d514f53a343f839a31d30b65d1bee0d678
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-05-15 09:08:06 +0100

    feat: add ProductCard component and display items on shop page

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  id: number;
+  productName: string;
+  price: number;
+  productURL: string;
+}
+
+const ProductCard = ({ id, productName, price, productURL }: Props) => {
+  return (
+    <div className="product">
+      <div className="product-img">
+        <img src={productURL} alt={productName} />
+      </div>
+      <div className="product-description">
+        <p>{productName}</p>
+        <p>£{price}</p>
+      </div>
+      <button className="btn-add-cart">Add To Cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import items from "../data/items.json";
+import ProductCard from "../components/ProductCard";
 
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop</h1>
+    <div className="shop">
+      <h1 className="shop-title">Shop</h1>
+
+      <div className="products">
+        {items.map((item) => {
+          return (
+            <ProductCard
+              key={item.id}
+              id={item.id}
+              productName={item.productName}
+              price={item.price}
+              productURL={item.productURL}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
